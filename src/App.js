f83770aba58b2c3bfefb2c1897e5dffb55e7382e
Gustav Modishi <mamosadi.gustav@gmail.com>
2023-06-20 22:08:44 +0200

    basic router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route  } from "react-router-dom"
+import NotFound from "./pages/NotFound";
+import Contact from "./pages/Contact";
+import Menu from "./pages/Menu";
+import Home from "./pages/Home";
+import Navbar from "./Navbar";
 
 function App() {
   return (
+
+  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Router>
+      <Navbar />
+     
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </Router>
+      
     </div>
-  );
+  
+  )
+    
+  ;
+  
+    
+  
 }
 
 export default App;
